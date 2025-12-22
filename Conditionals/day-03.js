10e9dd89df553e331c3297a14f1cec7c3200e705
chrlesdev <muhamadcharles0813@gmail.com>
2025-12-22 23:19:09 +0700

    test

---
+++ Conditionals/day-03.js
@@ -0,0 +1,11 @@
+function check(number) {
+  if (number < 3) {
+    return "number less than 3";
+  } else {
+    return "numbers is more than 3";
+  }
+}
+
+const values = [1, 2, 3, 4, 5];
+
+console.log(check(values));
