4c6223ed9b7614045733ff8bb6b6eb634d604a24
chrlesdev <charlescornelio0813@gmail.com>
2026-01-04 16:33:43 +0700

    project 2 pop quiz

---
+++ review_refactor_project/project_2/project2.js
@@ -0,0 +1,58 @@
+function checkAnswer() {}
+function nextQuestion() {}
+function showScore() {}
+
+const questions = [
+  {
+    question: "question 1 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+  {
+    question: "question 2 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+  {
+    question: "question 3 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+  {
+    question: "question 4 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+  {
+    question: "question 5 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+  {
+    question: "question 6 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+  {
+    question: "question 7 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+  {
+    question: "question 8 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+  {
+    question: "question 9 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+  {
+    question: "question 10 what which one is bigger 2 or 5",
+    options: [2, 5],
+    correct: 1,
+  },
+];
+
+console.log(questions[1]);
