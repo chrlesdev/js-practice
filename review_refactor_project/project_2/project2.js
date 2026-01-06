d29564f8cd200babd1c84d6c7e0ea24092920732
chrlesdev <charlescornelio0813@gmail.com>
2026-01-06 16:49:31 +0700

    work in progress

--- review_refactor_project/project_2/project2.js
+++ review_refactor_project/project_2/project2.js
@@ -1,58 +1,93 @@
 function checkAnswer() {}
-function nextQuestion() {}
 function showScore() {}
 
 const questions = [
   {
-    question: "question 1 what which one is bigger 2 or 5",
-    options: [2, 5],
+    question: "What is the capital of France?",
+    options: ["London", "Paris", "Berlin", "Madrid"],
     correct: 1,
   },
   {
-    question: "question 2 what which one is bigger 2 or 5",
-    options: [2, 5],
+    question: "How many days are in a week?",
+    options: ["5", "6", "7", "8"],
+    correct: 2,
+  },
+  {
+    question: "What color do you get when you mix blue and yellow?",
+    options: ["Green", "Orange", "Purple", "Red"],
+    correct: 0,
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    options: ["Venus", "Jupiter", "Mars", "Saturn"],
+    correct: 2,
+  },
+  {
+    question: "How many continents are there?",
+    options: ["5", "6", "7", "8"],
+    correct: 2,
+  },
+  {
+    question: "What is 10 x 5?",
+    options: ["50", "55", "45", "40"],
+    correct: 0,
+  },
+  {
+    question: "Which animal is known as the King of the Jungle?",
+    options: ["Tiger", "Elephant", "Lion", "Bear"],
+    correct: 2,
+  },
+  {
+    question: "How many legs does a spider have?",
+    options: ["6", "8", "10", "12"],
     correct: 1,
   },
   {
-    question: "question 3 what which one is bigger 2 or 5",
-    options: [2, 5],
-    correct: 1,
+    question: "What is the largest ocean on Earth?",
+    options: ["Atlantic", "Indian", "Arctic", "Pacific"],
+    correct: 3,
   },
   {
-    question: "question 4 what which one is bigger 2 or 5",
-    options: [2, 5],
-    correct: 1,
-  },
-  {
-    question: "question 5 what which one is bigger 2 or 5",
-    options: [2, 5],
-    correct: 1,
-  },
-  {
-    question: "question 6 what which one is bigger 2 or 5",
-    options: [2, 5],
-    correct: 1,
-  },
-  {
-    question: "question 7 what which one is bigger 2 or 5",
-    options: [2, 5],
-    correct: 1,
-  },
-  {
-    question: "question 8 what which one is bigger 2 or 5",
-    options: [2, 5],
-    correct: 1,
-  },
-  {
-    question: "question 9 what which one is bigger 2 or 5",
-    options: [2, 5],
-    correct: 1,
-  },
-  {
-    question: "question 10 what which one is bigger 2 or 5",
-    options: [2, 5],
+    question: "Which programming language is known for web styling?",
+    options: ["Python", "CSS", "Java", "C++"],
     correct: 1,
   },
 ];
 
-console.log(questions[1]);
+let { question } = questions[0];
+function questionFunc(questions) {
+  let index = 0;
+
+  return function () {
+    if (index >= questions.length) {
+      return null; // quiz finished
+    }
+
+    return questions[index++];
+  };
+}
+
+const nextQuestion = questionFunc(questions);
+console.log(nextQuestion()); // question 1
+console.log(nextQuestion()); // question 2
+console.log(nextQuestion()); // question 3
+
+// console.log();
+
+// function a() {
+//   let count = 0;
+//   return function () {
+//     return ++count;
+//   };
+// }
+
+// const counter = a();
+// console.log(counter() - 1);
+// console.log(counter() - 1);
+// console.log(counter() - 1);
+// console.log(counter() - 1);
+// console.log(counter() - 1);
+// console.log(counter() - 1);
+// console.log(counter() - 1);
+// console.log(counter() - 1);
+// console.log(counter() - 1);
