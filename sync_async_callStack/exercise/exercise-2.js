65bf9f8f31392f8842ae4bc5d78bc6aebef24a74
chrlesdev <charlescornelio0813@gmail.com>
2026-01-12 23:38:48 +0700

    exercise part 2

--- sync_async_callStack/exercise/exercise-2.js
+++ sync_async_callStack/exercise/exercise-2.js
@@ -31,3 +31,23 @@ function fetchComments(postId) {
     resolve({ postId: postId, count: commentCount });
   });
 }
+
+fetchUser(1)
+  .then((user) => {
+    fetchPosts(user.id)
+      .then((postId) => {
+        for (let i = 0; i < postId.length; i++) {
+          // console.log(postId[i].likes);
+          fetchComments(postId[i].id).then((comments) => console.log(comments));
+        }
+      })
+      .catch((err) => console.error(err));
+  })
+  .catch((err) => console.error(err));
+
+// fetchPosts(1)
+//   .then((postId) => console.log(postId))
+//   .catch((err) => console.error(err));
+// fetchComments(101)
+//   .then((comments) => console.log(comments))
+//   .catch((err) => console.error(err));
