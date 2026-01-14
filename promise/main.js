c9f67875f332a21c7f1bfe522010496d6e2513ab
chrlesdev <charlescornelio0813@gmail.com>
2026-01-14 14:54:47 +0700

    promises exercise

---
+++ promise/main.js
@@ -0,0 +1,71 @@
+// Your task: implement this
+function wait(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
+}
+
+// Should work like this:
+// wait(2000).then(() => console.log("2 seconds passed!"));
+
+function add5(num) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(num + 5);
+    }, 500);
+  });
+}
+
+function multiply2(num) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(num * 2);
+    }, 500);
+  });
+}
+
+function subtract3(num) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(num - 3);
+    }, 500);
+  });
+}
+
+// Chain them:
+// add5(10)
+//   .then(multiply2)
+//   .then(subtract3)
+//   .then((result) => console.log(result)); // Should log 27
+
+const urls = ["https://pokeapi.co/api/v2/pokemon/pikachu", "https://pokeapi.co/api/v2/pokemon/charizard", "https://pokeapi.co/api/v2/pokemon/bulbasaur"];
+
+async function getData() {
+  try {
+    const fetchApis = urls.map(async (e) => fetch(e));
+
+    const response = await Promise.all(fetchApis);
+
+    const jsonPromises = response.map((e) => {
+      return e.json();
+    });
+
+    const allData = await Promise.all(jsonPromises);
+
+    const names = allData.map((data) => data.species.name);
+
+    console.log(names);
+  } catch (error) {
+    console.error("fetch failed, error: ", error);
+  }
+}
+
+getData();
+
+// Your task:
+// 1. Fetch all 3 at once
+// 2. Get their names
+// 3. Log all 3 names
+// Hint: Use Promise.all() and map()
